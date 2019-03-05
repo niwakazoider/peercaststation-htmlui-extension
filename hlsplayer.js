2dcb4d01af3bed98d244e0c38e5898d1bb9d8c72
niwakazoider <1770418+niwakazoider@users.noreply.github.com>
2019-03-06 00:35:34 +0900

    test

---
+++ hlsplayer.js
@@ -0,0 +1,62 @@
+$(function(){
+  var script = $("<script src='https://cdn.jsdelivr.net/npm/hls.js@latest'>");
+  $("body").append(script);
+
+  var intervalID = setInterval(function(){
+    if($("#channels tr").size()>1 && typeof Hls != "undefined"){
+      clearInterval(intervalID);
+      onChannelLoad();
+    }
+  },1000);
+
+  function onChannelLoad(){
+    $("#channels tr").each(function(){
+      var tr = $(this);
+      var columnType = tr.find(".column-type");
+      var playUrl = columnType.find("a").attr("href").split("?")[0];
+      var channelID = playUrl.split("/")[2];
+      if(columnType.text()!="FLV"){
+        return;
+      }
+      var playButton = $("<i class='hls-play-button icon-play-circle'>");
+      tr.find(".column-play").append(playButton);
+      playButton.click(function(){
+        var video_id = "video_"+channelID;
+        var newtr = $("<tr><td class='hls-player'><video width='640' controls='true'></td></tr>");
+        newtr.find("video").attr("id",video_id);
+        newtr.insertAfter(tr);
+        $(".hls-player").css("display","inline-table");
+        playHls(video_id, "/pls/"+channelID+".m3u8");
+      });
+    });
+    $("#channels .column-play").css("width", "30px");
+    $("#channels .hls-play-button").css("margin-left","6px");
+    $("#channels .hls-play-button").css("cursor","pointer");
+  }
+
+  function getHlsPlayer(id){
+    var video = document.getElementById(id);
+    if(!video){
+      video = document.createElement("video");
+      document.body.appendChild(video);
+    }
+    return video;
+  }
+
+  function playHls(id, src){
+    var video = getHlsPlayer(id);
+    if(Hls.isSupported()) {
+      var hls = new Hls();
+      hls.loadSource(src);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.play();
+      });
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = src;
+      video.addEventListener("loadedmetadata",function() {
+        video.play();
+      });
+    }
+  }
+});
